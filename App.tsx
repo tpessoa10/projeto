23945db1ca00e9632636f812338a6b3921b00d73
RenanLira-Aluno <catce.20191ads0401@aluno.ifpi.edu.br>
2023-10-30 19:43:08 -0300

    geolocation

--- App.tsx
+++ App.tsx
@@ -4,9 +4,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { HomePage } from './src/screen/Home';
 
+import * as Location from 'expo-location'
+import { useEffect, useState } from 'react';
+
 const stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied')
+        return
+      }
+
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+
+    })()
+  }, [])
+
   return (
     <NavigationContainer>
       <stack.Navigator initialRouteName='Home'>
